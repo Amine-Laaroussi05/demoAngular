5304ae833ae4a27637e9082569ab489418a5e33d
AlexDvsSemifir <alexandre.devos@semifir.com>
2022-06-09 14:49:34 +0200

    feat: Ajout carte de visite :lipstick:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { UserComponent } from './components/user/user.component';
+import { CarteVisiteComponent } from './components/carte-visite/carte-visite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    UserComponent
+    UserComponent,
+    CarteVisiteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
