9b80e888f9203e34972b766a0da58ce6f278e85e
AlexDvsSemifir <alexandre.devos@semifir.com>
2022-06-09 13:55:56 +0200

    feat: Ajout first componenent et user component :sparkles:

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent implements OnInit {
+
+  name: string = "Alex";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
